a7cc68888f91e71a629c0b2c8eb9a700e458e756
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-10-21 00:36:22 +0300

    my first component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,19 @@
+import { Profile } from './Profile/Profile';
+import user from '../user.json';
+
 export const App = () => {
   return (
-    <div
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+  );
+};
+
+/* <div
       style={{
         height: '100vh',
         display: 'flex',
@@ -10,7 +23,5 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
-    </div>
-  );
-};
+      Hello world!
+    </div> */
