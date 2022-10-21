24599dc38ef8b860a827576303d00be524a55a4d
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-10-22 02:45:38 +0300

    added FriendList component

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friend_list}>
+      {friends.map(friend => (
+        <li key={friend.id} className={css.item}>
+          <span className={css.status}></span>
+          <img
+            className={css.avatar}
+            src={friend.avatar}
+            alt="User avatar"
+            width="48"
+          />
+          <p className={css.name}>{friend.name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  stats: PropTypes.array,
+};
